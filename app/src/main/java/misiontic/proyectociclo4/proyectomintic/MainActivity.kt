ed355504b43d26d6ce9e3111e9f97d892d22e253
Samuel T <samueltaborda231@gmail.com>
2021-12-07 12:15:36 -0500

    Proyecto de mintic

---
+++ app/src/main/java/misiontic/proyectociclo4/proyectomintic/MainActivity.kt
@@ -0,0 +1,11 @@
+package misiontic.proyectociclo4.proyectomintic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
